3bc81f959c10ed7c876a5ef75df0545c66c168f9
kpion <konradpapala@gmail.com>
2024-11-22 04:13:55 +0100

    Clear some stuff

--- src/Test.php
+++ src/Test.php
@@ -4,7 +4,6 @@ namespace Kpion\Spaghetti;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Create an instance of Spaghetti and load the specified .phd file
-$sp = new Spaghetti($argv);
-require_once $sp->inputFile(false);
+# $sp = new Spaghetti($argv);
+# require_once $sp->inputFile(false);
 
